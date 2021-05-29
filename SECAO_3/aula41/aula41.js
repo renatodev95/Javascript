e56392b936497cfa8bbc1bb143ce344143ac6703
renatodev95 <renatodev95@gmail.com>
2021-05-28 21:19:57 -0300

    (Exercício) - Função que retorna o maior valor

---
+++ SECAO_3/aula41/aula41.js
@@ -0,0 +1,14 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+
+// Usando função clássica
+function maiorValor1(num1, num2) {
+  return Math.max(num1, num2);
+}
+
+console.log(maiorValor1(100, 100));
+
+// Usando arrow function
+const maiorValor2 = (a, b) => Math.max(a, b);
+
+
+console.log(maiorValor2(150, 33));
