780f492d3338bdefb2ed8e4ab6a303c52d9ffb8a
renatodev95 <renatodev95@gmail.com>
2021-05-28 21:20:43 -0300

    (Exercício) - Função ePaisagem

---
+++ SECAO_3/aula42/exercicio.js
@@ -0,0 +1,7 @@
+// Escreva uma função chamada ePaisagem
+// que recebe dois argumentos, largura e altura de uma image (number).
+// Retorne true se a imagem estiver no modo paisegem.
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(1920, 1080));
