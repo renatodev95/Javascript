2567add3a5be590771a76984f614e8eb3bd85f94
renatodev95 <renatodev95@gmail.com>
2021-06-04 10:07:50 -0300

    Calculadora com Função Construtora

--- SECAO_4/aula59/assets/js/main.js
+++ SECAO_4/aula59/assets/js/main.js
@@ -4,72 +4,66 @@
 function Calculadora() {
   this.display = document.querySelector(".display");
 
+  // A calculadora inicia e faz as operações baseada nos clicks e na captura do ENTER
   this.inicia = () => {
-    this.cliqueBotoes();
-    this.pressionaEnter();
+    this.capturaCLiques();
+    this.capturaEnter();
   };
 
-  this.pressionaEnter = () => {
-    this.display.addEventListener("keyup", (e) => {
+  // Adicionando a função Enter para realizar conta, além do botão de igual
+  this.capturaEnter = () => {
+    this.display.addEventListener("keypress", (e) => {
       if (e.keyCode === 13) {
         this.realizaConta();
       }
     });
   };
 
-  this.realizaConta = () => {
-    let conta = this.display.value;
+  // Capturando os clicks nos botões da página html
+  this.capturaCLiques = () => {
+    document.addEventListener("click", (event) => {
+      const el = event.target;
+      if (el.classList.contains("btn-num")) this.addNumDisplay(el);
+      if (el.classList.contains("btn-clear")) this.clear();
+      if (el.classList.contains("btn-del")) this.del();
+      if (el.classList.contains("btn-eq")) this.realizaConta();
+    });
+  };
 
+  // Retornando o resultado para dentro do display se a conta for válida
+  this.realizaConta = () => {
     try {
-      conta = eval(conta);
+      const conta = eval(this.display.value);
 
       if (!conta) {
         alert("Conta inválida");
         return;
       }
 
-      this.display.value = String(conta);
-    } catch (e) {
+      this.display.value = conta;
+    } catch (error) {
       alert("Conta inválida");
       return;
     }
   };
 
-  this.clearDisplay = () => {
-    this.display.value = "";
+  // Adicionando os valores do input ao display
+  this.addNumDisplay = (el) => {
+    this.display.value += el.innerText;
+    // Cada vez que um botão é clicado, o foco volta para o display
+    // evitando que o botão Enter seja compreendido como um novo click pelo navegador
+    this.display.focus();
   };
 
-  this.apagaUm = () => {
-    this.display.value = this.display.value.slice(0, -1);
-  };
+  // Limpando todo o display da calculadora
+  this.clear = () => (this.display.value = "");
 
-  this.cliqueBotoes = () => {
-    document.addEventListener("click", (e) => {
-      const element = e.target;
-
-      if (element.classList.contains("btn-num")) {
-        this.btnParaDisplay(element.innerText);
-      }
-
-      if (element.classList.contains("btn-clear")) {
-        this.clearDisplay();
-      }
-
-      if (element.classList.contains("btn-del")) {
-        this.apagaUm();
-      }
-
-      if (element.classList.contains("btn-eq")) {
-        this.realizaConta();
-      }
-    });
-  };
-
-  this.btnParaDisplay = (valor) => {
-    this.display.value += valor;
-  };
+  // Limpando um valor de cada vez
+  this.del = () => (this.display.value = this.display.value.slice(0, -1));
 }
 
+// A constante recebe um novo molde do construtor ` new Calculadora()`
 const calculadora = new Calculadora();
 
+// Iniciando a calculadora de fato
 calculadora.inicia();
