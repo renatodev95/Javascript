205c8bc34a636164d0d631f0e96373bac1379f2e
renato <renatodev95@gmail.com>
2021-06-10 09:24:02 -0300

    POO --> Getters e Setters

---
+++ SECAO_7/aula81/aula81.js
@@ -0,0 +1,41 @@
+/* POO --> Getters e Setters */
+
+// Criando um symbol para proteger a propriedade velocidade de alterações externas
+const _velocidade = Symbol('velocidade');
+class Carro {
+  constructor(nome) {
+    this.nome = nome;
+    this[_velocidade] = 0;
+  }
+
+  // Criando um setter para tornar possível a alteração da propriedade que está protegida
+  set velocidade(valor) {
+    console.log('SETTER');
+    if (typeof valor !== 'number') return;
+    if (valor > 100 || valor <= 0) return;
+    this[_velocidade] = valor;
+  }
+
+  // Criando um getter para retornar a propriedade "velocidade"
+  get velocidade() {
+    console.log('GETTER');
+    return this[_velocidade];
+  }
+
+  acelerar() {
+    const limite = 100;
+    if (this[_velocidade] >= limite) return;
+    this[_velocidade]++;
+  }
+
+  freiar() {
+    if (this[_velocidade] <= 0) return;
+    this[_velocidade]--;
+  }
+}
+
+const c1 = new Carro('Fusca');
+
+console.log(c1.velocidade);
+c1.velocidade = 33;
+console.log(c1.velocidade);
