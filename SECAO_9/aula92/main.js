d59be0d74adb18a87afbe32b512852301c669d71
renatodev95 <renatodev95@gmail.com>
2021-06-15 07:15:00 -0300

    Transpilador BABEL

--- SECAO_9/aula92/main.js
+++ SECAO_9/aula92/main.js
@@ -1,4 +1,9 @@
-const nome = 'Renato';
-const obj = { nome };
-const novoObj = { ...obj };
-console.log(novoObj);
+class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+}
+
+/* Transpilador JavaScript de código aberto e gratuito, o Babel foi lançado sobre a licença MIT e possui como uma das principais características converter código JavaScript atual em uma versão que o navegador possa executar. 
+Multiplataforma, é amplamente utilizado por desenvolvedores que necessitarem converter recursos da linguagem JavaScript para uma versão que o navegador compreenda. */
