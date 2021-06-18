2aec95e1bb0a0a6acc1ab01fcb87b39a97c70ad3
renatodev95 <renatodev95@gmail.com>
2021-06-18 09:11:16 -0300

    Node -> módulos #2

---
+++ SECAO_10 (Node)/aula2/Z/mod2.js
@@ -0,0 +1,4 @@
+// Para fazer um caminho de retorno nos diretórios, utilizamos ../
+const Cachorro = require('../B/C/D/mod');
+
+module.exports = Cachorro;
---
+++ SECAO_10 (Node)/aula2/app.js
@@ -0,0 +1,7 @@
+const Cachorro = require('./Z/mod2.js');
+const c1 = new Cachorro('Rex');
+c1.latir();
+
+// Variáveis convenientes para visualizar os caminhos
+console.log(__filename); // exibe o caminho do ARQUIVO atual
+console.log(__dirname); // exibe o caminho da PASTA atual
