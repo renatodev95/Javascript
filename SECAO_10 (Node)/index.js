49d48946437017ecb1214def03b032f501890f0a
renatodev95 <renatodev95@gmail.com>
2021-06-18 10:33:35 -0300

    Listando arquivos com FS e Recursão mútua

---
+++ SECAO_10 (Node)/index.js
@@ -0,0 +1,35 @@
+// Listando arquivos com FS e Recursão mútua
+
+const fs = require('fs').promises;
+const path = require('path');
+
+async function readdir(rootDir) {
+  rootDir = rootDir || path.resolve(__dirname);
+  const files = await fs.readdir(rootDir);
+  walk(files, rootDir);
+}
+
+async function walk(files, rootDir) {
+  for (let file of files) {
+    const fileFullPath = path.resolve(rootDir, file);
+    const stats = await fs.stat(fileFullPath);
+
+    // Usando REGEX para não listar arquivos da pasta .git
+    if (/\.git/g.test(fileFullPath)) continue;
+    if (/node_modules/g.test(fileFullPath)) continue;
+
+    // Aqui está a recursão para caminhar sobre os arquivos que forem pastas (diretórios)
+    if (stats.isDirectory()) {
+      readdir(fileFullPath);
+      continue;
+    }
+
+    // Usando regex para listar apenas arquivos .css e .html
+    if (!/\.html$/g.test(fileFullPath) && !/\.css$/g.test(fileFullPath))
+      continue;
+
+    console.log(fileFullPath);
+  }
+}
+
+readdir('/home/renato/Documents/UDEMY/Javascript/');
