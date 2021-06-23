9c07d235196b582ed59de531fbf0f0506462eb4e
renato <renatodev95@gmail.com>
2021-06-22 21:16:48 -0300

    EXPRESS --> Instalação do NODEMON

---
+++ SECAO_10/aula7/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+//          Criar    Ler     Atualizar       Apagar
+// CRUD -> CREATE,  READ,     UPDATE,        DELETE
+//          POST    GET        PUT           DELETE
+
+// http://meusite.com/  <- GET -> Entregue a página /
+// http://meusite.com/sobre  <- GET -> Entregue a página /sobre
+// http://meusite.com/contato  <- GET -> Entregue a página /contato
+
+// Criando uma rota que vai entregar um formulário quando a raiz do site for acessada
+app.get('/', (req, res) => {
+  res.send(`
+  <form action="/" method="POST">
+  Nome do cliente: <input type="text" name="nome">
+  <button>Novo mundo</button>
+  `);
+});
+
+// Criando um método POST. Quando uma requisição do tipo post for feita, a mensagem é exibida
+app.post('/', (req, res) => {
+  res.send('Recebi o formulário');
+});
+
+// Criando uma rota para a parte de contato do site ('/')
+app.get('/contato', (req, res) => {
+  res.send('Obrigado por entrar em contato conosco.');
+});
+
+// Nosso aplicativo irá escutar tudo que for feito através da porta 3000
+app.listen(3000, () => {
+  console.log('Acessar o site http://localhost:3000');
+  console.log('Servidor executando na porta 3000.');
+});
